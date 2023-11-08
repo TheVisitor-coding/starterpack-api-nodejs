9070e74cb508f99c80f1fc11f0e86cc52514f04e
MatteoRossi_Dev <matteorossiroy.pro@gmail.com>
2023-11-08 15:47:39 +0100

    update routes to handle user id parameter

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,29 +1,57 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var db = require('../db');
+var db = require("../db");
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
-
+router.get("/", async function (req, res,) {
   // #swagger.summary = "Page d'accueil"
 
   const conn = await db.mysql.createConnection(db.dsn);
 
   try {
-    
-    const [rows] = await conn.execute('SELECT * FROM User');
+    const [rows] = await conn.execute("SELECT * FROM User");
 
-    const users = rows.map(element => {
+    const users = rows.map((element) => {
       return {
-        firstName: element.first_name
-      }
+        firstName: element.first_name,
+      };
     });
-    res.render('index', { title: 'RESTful web api', 'users': users });
-
+    res.render("index", { title: "RESTful web api", users: users });
   } catch (error) {
-    console.error('Error connecting: ' + error.stack);
-    res.status(500).json({ "msg": "Nous rencontrons des difficultés, merci de réessayer plus tard." });
+    console.error("Error connecting: " + error.stack);
+    res.status(500).json({
+      msg: "Nous rencontrons des difficultés, merci de réessayer plus tard.",
+    });
+  }
+});
 
+router.get("/users/:id", async function (req, res) {
+  const conn = await db.mysql.createConnection(db.dsn);
+  const id = req.params.id;
+  if (Number(id)) {
+    try {
+      const row = await conn.execute("SELECT * FROM User WHERE id = ?", [id]);
+      console.log("ID from URL:", id);
+
+      const user = {
+        firstName: row[0][0].first_name,
+      };
+
+      res.render("user", {
+        title: "Fiche User",
+        user: user,
+        id: id,
+      });
+    } catch (error) {
+      console.error("Error connecting: " + error.stack);
+      res.status(500).json({
+        msg: "Identifiant Inconnu",
+      });
+    }
+  } else {
+    res.status(400).json({
+      msg: "Identifiant Invalide",
+    });
   }
 });
 
